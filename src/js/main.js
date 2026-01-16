a0cf0b117a7eddb355dc9eb6241643822499c384
Danil Tsybulin <https://git.staffjet.ru/>
2026-01-16 16:23:43 +0600

    Fix folder structure

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// css
+import "../scss/index.scss";
+import "modern-css-reset";
+
+// js
+import "./modules/menuBurger";
+import "./modules/currentTime";
+import "./modules/worksViewMode";
+import "./modules/testimonialsTrack";
+
+import "./modules/base";
