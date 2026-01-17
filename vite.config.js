553d07987f83b3ed0d1c88807c60c8a811ea6f41
Danil Tsybulin <https://git.staffjet.ru/>
2026-01-17 08:58:39 +0600

    Add multiple HTML pages to the build

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import fs from "fs";
+
+const pages = fs
+  .readdirSync(__dirname)
+  .filter((file) => file.endsWith(".html"))
+  .reduce((acc, file) => {
+    const name = file.replace(".html", "");
+    acc[name] = resolve(__dirname, file);
+    return acc;
+  }, {});
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: pages,
+    },
+  },
+});
